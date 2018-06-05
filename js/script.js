286ccde3099c9f76d2ec974762e70033487b4079
Toussaint Dimitry <dimitrytoussaint2@gmail.com>
2018-06-05 13:29:26 +0200

    je sais aps quoi ecrire

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 var listCard = document.getElementsByClassName('carte');
 var faceCard = document.getElementsByClassName('facedown');
-
+var selection = "";
+var cpt = 0;
 
 for (var i = 0; i < faceCard.length; i++) {
     myFaceC = faceCard[i];
     myFaceC.addEventListener('click', function(){
+        this.className = "visible";
+      if(cpt = 2 ) {
+          document.getElementsByClassName('visible')[1].style.visibility = "visible";
+          document.getElementsByClassName('visible')[2].style.visibility = "visible";
+          cpt=0;
+      }
+      cpt++;
       this.style.visibility = "hidden";
 });
 }
+
+
+
+
+
+
+
+
+
+
+// if( selection.src = listCard[i].src ) {
+//
+// }
